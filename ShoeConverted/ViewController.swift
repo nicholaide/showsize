e891e447404f4f0c1989f1b1fde7b15736a897d9
Nicholai de Guzman <nicholaideguzman@nis-imac.dhcp.swmed.org>
2014-12-30 16:05:08 -0600

    after hooking outlets and actions

--- ShoeConverted/ViewController.swift
+++ ShoeConverted/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +25,7 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+    }
 }
 
